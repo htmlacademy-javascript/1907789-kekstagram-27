c54fbfee1bfd007de57985e2962feacb813f927a
ruslantz <antz85@mail.ru>
2022-10-06 13:36:25 +0300

    Исправляет ошибки дз

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-function getRandomIntInclusive(a, b) {
-  if (a >= b) {
-    return 'Данные на входе не корректны';
-  } else if (!Number(a || b) || (a || b) < 0) {
+const getRandomIntInclusive = (a, b) => {
+  if (!Number(a || b) || (a || b) < 0 || a >= b) {
     return NaN;
   }
+
   const min = Math.ceil(a);
   const max = Math.floor(b);
+
   return Math.round(Math.random() * (max - min + 1)) + min;
-}
+};
 
 getRandomIntInclusive(7, 5);
 
+const checkStringLength = (string, maxString) => string.length <= maxString;
 
-function checkStringLenght(string, maxString) {
-  return string.lenght <= maxString;
-}
-
-checkStringLenght('test', 12);
+checkStringLength('строка', 12);
