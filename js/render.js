ebeebf2a43579627dbdbefb22481e0ba6330a8e1
ruslantz <antz85@mail.ru>
2022-11-10 16:28:49 +0300

    Добавляет alt

--- js/render.js
+++ js/render.js
@@ -10,10 +10,11 @@ const picturesFragment = document.createDocumentFragment();
 
 
 const createPictures = (picturesData) => {
-  picturesData.forEach(({url, likes, comments}) => {
+  picturesData.forEach(({url, likes, comments, description}) => {
     const picture = randomUserPictureTemplate.cloneNode(true);
     picture.querySelector('.picture__img').src = url;
-    picture.querySelector('.picture__comments').textContent = comments;
+    picture.querySelector('.picture__img').alt = description;
+    picture.querySelector('.picture__comments').textContent = comments.length;
     picture.querySelector('.picture__likes').textContent = likes;
     picturesFragment.append(picture);
   });
