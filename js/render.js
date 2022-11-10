441d67c393ce60cc43a683c79baa974c1a3bfa25
ruslantz <antz85@mail.ru>
2022-11-10 15:04:21 +0300

    Делает модуль render

--- js/render.js
+++ js/render.js
@@ -9,8 +9,8 @@ const randomUserPictureTemplate = document.querySelector('#picture')
 const picturesFragment = document.createDocumentFragment();
 
 
-const createPictures = (b) => {
-  b.forEach(({url, likes, comments}) => {
+const createPictures = (picturesData) => {
+  picturesData.forEach(({url, likes, comments}) => {
     const picture = randomUserPictureTemplate.cloneNode(true);
     picture.querySelector('.picture__img').src = url;
     picture.querySelector('.picture__comments').textContent = comments;
